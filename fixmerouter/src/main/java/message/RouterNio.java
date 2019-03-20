5a74d1dbd0fb5ccc480d22a96588c82b6bc4070a
Taras <snoopgtg@gmail.com>
2019-03-21 00:46:10 +0200

    router is working on two ports: 5000 and 5001

--- fixmerouter/src/main/java/message/RouterNio.java
+++ fixmerouter/src/main/java/message/RouterNio.java
@@ -1,24 +1,75 @@
 package message;
 
+
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class RouterNio {
 
+    private final int portForBrocker;
+    private final int portForMarket;
+
     public static void main(String[] args) throws InterruptedException {
+        new RouterNio(5000, 5001).run();
+
+    }
+
+
+    public RouterNio(int portForBrocker, int portForMarket) {
+        this.portForBrocker = portForBrocker;
+        this.portForMarket = portForMarket;
+    }
+
+
+    public void run() throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(2);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(10);
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap()
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new RouterNioInitializer());
+            System.out.println("The server at port " + portForBrocker + " is ready.");
+            ChannelFuture futureBrocker = bootstrap.bind(portForBrocker);
+            System.out.println("The server at port " + portForMarket + " is ready.");
+            ChannelFuture futureMarket = bootstrap.bind(portForMarket);
+            futureBrocker.sync().channel().closeFuture().sync();
+            futureMarket.sync().channel().closeFuture().sync();
+        }
+        finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
+
+/*
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class RouterNio {
+
+    public static void main(String[] args) */
+/*throws InterruptedException*//*
+ {
        RouterNio routerNio = new RouterNio(5000);
        RouterNio routerMarket = new RouterNio(5001);
 
-        Thread threadRouterBroker = new Thread();
-        routerNio.run();
-        Thread threadRouterMarket = new Thread();
-        routerMarket.run();
-//        Thread.currentThread().join();
     }
 
     private final int port;
+    private List<ChannelFuture> futureBrocker = new ArrayList<Ch>();
 
     public RouterNio(int port) {
         this.port = port;
@@ -34,6 +85,7 @@ public class RouterNio {
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new RouterNioInitializer());
 
+            System.out.println("The server at port " + port + " is ready.");
             bootstrap.bind(port).sync().channel().closeFuture().sync();
         }
         finally {
@@ -42,3 +94,4 @@ public class RouterNio {
         }
     }
 }
+*/
--- fixmerouter/src/main/java/message/RouterNioHandler.java
+++ fixmerouter/src/main/java/message/RouterNioHandler.java
@@ -9,15 +9,22 @@ import io.netty.channel.group.DefaultChannelGroup;
 public class RouterNioHandler extends ChannelInboundMessageHandlerAdapter<String> {
 
     private static final ChannelGroup channels = new DefaultChannelGroup();
+    private static final ChannelGroup channelsMarket = new DefaultChannelGroup();
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
         Channel incoming = ctx.channel();
+        Channel incomingM = ctx.channel();
         for (Channel channel : channels) {
             channel.write("Server - " + incoming.remoteAddress() + " has joined!\n");
             channel.flush();
         }
+        for (Channel channel : channelsMarket) {
+            channelsMarket.write("Server - " + incoming.remoteAddress() + " has joined!\n");
+            channelsMarket.flush();
+        }
         channels.add(ctx.channel());
+        channelsMarket.add(ctx.channel());
     }
 
     @Override
